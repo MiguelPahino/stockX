5e60f67880eccee0d701013e1a3eb7621b3d143f
MiguelPahino <mpahinogutierrez@gmail.com>
2026-05-09 13:24:23 +0200

    feat(Sale): :sparkles: Añadir el módulo nuevo para implementar una oferta nueva

---
+++ app/src/main/java/edu/teamrocket/offer/Sale.java
@@ -0,0 +1,36 @@
+package edu.teamrocket.offer;
+
+public class Sale implements Offer{
+    private String size = new String();
+    private Integer price = 0;
+
+    Sale(String size, Integer price){
+        this.size = size;
+        this.price = price;
+    }
+
+    @Override
+    public String size() {
+        return this.size;
+    }
+
+    @Override
+    public int value() {
+        return this.price;
+    }
+
+    @Override
+    public int compareTo(Offer oferta) {
+        return Integer.compare(this.price, oferta.value());
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder build = new StringBuilder();
+        build.append("Bid\n Size: ");
+        build.append(size());
+        build.append("\nAsk: ");
+        build.append(value());
+        return build.toString();        
+    }
+}
