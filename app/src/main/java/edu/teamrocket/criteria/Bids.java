7dd610084171251099510e8767cebc34c642a865
MiguelPahino <mpahinogutierrez@gmail.com>
2026-05-09 13:20:59 +0200

    feat(Ask): :sparkles: Añadir el nuevo módulo Ask para implementar una oferta

---
+++ app/src/main/java/edu/teamrocket/criteria/Bids.java
@@ -0,0 +1,16 @@
+package edu.teamrocket.criteria;
+
+import java.util.List;
+
+import edu.teamrocket.item.Item;
+import edu.teamrocket.offer.Offer;
+
+public class Bids implements Criteria {
+    
+    Bids(){};
+
+    @Override
+    public List<Offer> checkCriteria(Item item) {
+        
+    }
+}
---
+++ app/src/main/java/edu/teamrocket/offer/Ask.java
@@ -0,0 +1,36 @@
+package edu.teamrocket.offer;
+
+public class Ask implements Offer{
+    private String size = new String();
+    private Integer ask = 0;
+
+    Ask(String size, Integer ask){
+        this.size = size;
+        this.ask = ask;
+    }
+
+     @Override
+    public String size() {
+        return this.size;
+    }
+
+    @Override
+    public int value() {
+        return this.ask;
+    }
+
+    @Override
+    public int compareTo(Offer oferta) {
+        return Integer.compare(this.ask, oferta.value());
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder build = new StringBuilder();
+        build.append("Bid\n Size: ");
+        build.append(size);
+        build.append("\nAsk: ");
+        build.append(ask);
+        return build.toString();        
+    }
+}
