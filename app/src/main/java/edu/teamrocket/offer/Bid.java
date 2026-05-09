cf7bc88800402f2453a914e5d945a9b3e28921f1
MiguelPahino <mpahinogutierrez@gmail.com>
2026-05-09 13:16:33 +0200

    feat(Bid): :sparkles: Añadir el módulo para la oferta Bid

---
+++ app/src/main/java/edu/teamrocket/offer/Bid.java
@@ -0,0 +1,37 @@
+package edu.teamrocket.offer;
+
+public class Bid implements Offer {
+    
+    private String size = new String();
+    private Integer bid = 0;
+
+    Bid(String size, int bid){
+        this.size = size;
+        this.bid = bid;
+    }
+
+    @Override
+    public String size() {
+        return this.size;
+    }
+
+    @Override
+    public int value() {
+        return this.bid;
+    }
+
+    @Override
+    public int compareTo(Offer oferta) {
+        return Integer.compare(this.bid, oferta.value());
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder build = new StringBuilder();
+        build.append("Bid\n Size: ");
+        build.append(size);
+        build.append("\nBid: ");
+        build.append(bid);
+        return build.toString();        
+    }
+}
